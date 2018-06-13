d1b976eff78d37ca2defd3945d3939a6105320a9
Shivam Patel <spatel@KBPDEVDSPATE1369.local>
2018-06-13 09:11:54 -0400

    added whitenoise

--- SabiaWebsite/settings.py
+++ SabiaWebsite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,8 @@ STATICFILES_FINDERS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'SabiaWebsite/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATIC_ROOT = '/Users/spatel/SabiaWebsite/SabiaWebsite/SabiaWebsite'
